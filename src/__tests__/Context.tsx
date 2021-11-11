716a8e3c0df67bd010043cee4ecf33bafc364d25
pure180 <info@daniel-pfisterer.de>
2021-11-11 08:51:10 +0100

    Describe and add some more tests

--- src/__tests__/Context.tsx
+++ src/__tests__/Context.tsx
@@ -1,55 +1,126 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
-import { unmountComponentAtNode, render } from 'react-dom';
-
+import { render, unmountComponentAtNode } from 'react-dom';
 import { act } from 'react-dom/test-utils';
+
+import { createAppStateContext } from '../Context';
+import { createAppStateProvider } from '../Provider';
+import { AppContextValue, AppState } from '../Types';
 import TestApp from './fixtures/App';
 import { fixtureState, TestStateKeys } from './fixtures/State';
 
 const Fixture: React.FunctionComponent = () => <TestApp />;
+describe('Test Context-API', () => {
+  describe('Basic component rendering', () => {
+    let container: HTMLDivElement | null;
+    let paragraph: HTMLParagraphElement | null | undefined;
 
-describe('Context', () => {
-  let container: HTMLDivElement | null;
-  let paragraph: HTMLParagraphElement | null | undefined;
+    beforeEach(() => {
+      container = document.createElement('div');
+      document.body.appendChild(container);
 
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
+      act(() => {
+        render(<Fixture />, container);
+      });
 
-    act(() => {
-      render(<Fixture />, container);
+      paragraph = container?.querySelector('p');
     });
 
-    paragraph = container?.querySelector('p');
-  });
-
-  afterEach(() => {
-    if (container) {
-      unmountComponentAtNode(container);
-      container.remove();
-    }
-    container = null;
-  });
-
-  it('renders with fully initialized application state', () => {
-    expect(paragraph?.textContent).toBe(
-      fixtureState[TestStateKeys.Test].state.test,
-    );
-  });
-
-  it('changes the application state and renders the correct value', () => {
-    const button = container?.querySelector('button');
-
-    expect(button?.innerHTML).toBe('Test Button');
-
-    act(() => {
-      button?.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    afterEach(() => {
+      if (container) {
+        unmountComponentAtNode(container);
+        container.remove();
+      }
+      container = null;
     });
 
-    paragraph = container?.querySelector('p');
+    it('renders with fully initialized application state', () => {
+      expect(paragraph?.textContent).toBe(
+        fixtureState[TestStateKeys.Test].state.test,
+      );
+    });
 
-    expect(paragraph?.textContent).toBe('new_test_value');
-    expect(paragraph?.textContent).not.toBe(
-      fixtureState[TestStateKeys.Test].state.test,
-    );
+    it('changes the application state and renders the correct value', () => {
+      const button = container?.querySelector('button');
+
+      expect(button?.innerHTML).toBe('Test Button');
+
+      act(() => {
+        button?.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+      });
+
+      paragraph = container?.querySelector('p');
+
+      expect(paragraph?.textContent).toBe('new_test_value');
+      expect(paragraph?.textContent).not.toBe(
+        fixtureState[TestStateKeys.Test].state.test,
+      );
+    });
+  });
+
+  describe('Functionality and Context consumer rendering', () => {
+    let container: HTMLDivElement | null;
+    let Context: React.Context<AppContextValue<Object, unknown, unknown>>;
+    let Provider: React.FunctionComponent<
+      React.PropsWithChildren<{
+        state: AppState<Object, unknown, unknown>;
+      }>
+    >;
+
+    const testString = 'It Works';
+    let TestComponent: React.FunctionComponent<{ renderConsumer: boolean }>;
+
+    beforeEach(() => {
+      container = document.createElement('div');
+      document.body.appendChild(container);
+
+      Context = createAppStateContext();
+      Provider = createAppStateProvider(Context);
+
+      // eslint-disable-next-line react/display-name
+      TestComponent = ({ renderConsumer }) => (
+        <Provider state={fixtureState as AppState<Object, unknown, unknown>}>
+          {!renderConsumer && <>{testString}</>}
+          {renderConsumer && (
+            <Context.Consumer>
+              {(ctx) => <>{typeof ctx.dispatch}</>}
+            </Context.Consumer>
+          )}
+        </Provider>
+      );
+    });
+
+    afterEach(() => {
+      if (container) {
+        unmountComponentAtNode(container);
+        container.remove();
+      }
+      container = null;
+    });
+
+    it('should create the context object without errors', () => {
+      expect(Context.Provider.$$typeof.toString()).toBe(
+        'Symbol(react.provider)',
+      );
+      expect(Context.Consumer.$$typeof.toString()).toBe(
+        'Symbol(react.context)',
+      );
+    });
+
+    it('should render the previously created provider without errors', () => {
+      act(() => {
+        render(<TestComponent renderConsumer={false} />, container);
+      });
+
+      expect(container?.textContent).toBe(testString);
+    });
+
+    it('should render the context consumer without errors', () => {
+      act(() => {
+        render(<TestComponent renderConsumer={true} />, container);
+      });
+
+      expect(container?.textContent).toBe('function');
+    });
   });
 });
