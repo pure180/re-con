e813890bcbd9913c5ee9ae1acd2b76c629555276
pure180 <info@daniel-pfisterer.de>
2021-11-11 09:05:17 +0100

    Fix running test in build folder

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: { '@exmpl/(.*)': '<rootDir>/src/$1' },
-  modulePathIgnorePatterns: ['<rootDir>/src/__tests__/fixtures'],
+  testPathIgnorePatterns: [
+    '<rootDir>/src/__tests__/fixtures/',
+    '<rootDir>/build',
+  ],
 };
